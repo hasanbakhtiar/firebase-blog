79fa04890c69bc254c1bad3bf683c7b2f743effd
Hasan Bakhtiar <52384595+hasanbakhtiar@users.noreply.github.com>
2023-01-05 20:35:06 +0400

    Update firebaseConfig.jsx

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/database'; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDxVd-HJ1MoVEBgN3B0cyyCz6ehXvXxb0U",
-    authDomain: "codeislife-f1ab6.firebaseapp.com",
-    databaseURL: "https://codeislife-f1ab6-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "codeislife-f1ab6",
-    storageBucket: "codeislife-f1ab6.appspot.com",
-    messagingSenderId: "797473712224",
-    appId: "1:797473712224:web:1140ad6c3edaaafc2f3b05"
+      apiKey: "AIzaSyCDADfR8kHKeLqSgO5K5x301P7cd32iFok",
+  authDomain: "techno-brain-866f4.firebaseapp.com",
+  databaseURL: "https://techno-brain-866f4-default-rtdb.firebaseio.com",
+  projectId: "techno-brain-866f4",
+  storageBucket: "techno-brain-866f4.appspot.com",
+  messagingSenderId: "393914914879",
+  appId: "1:393914914879:web:36dc4879a27a46ebe0808a"
 };
 
 // Initialize Firebase
